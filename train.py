283637a38725d0190d876fee68597b83e5de51ca
GreenLeo <nguyetnguyen15802@gmail.com>
2022-03-10 13:18:34 +0700

    update process_data func

--- train.py
+++ train.py
@@ -22,10 +22,11 @@ def process_data(data_path):
     df.loc[:, 'word'] = df['word'].fillna("")
     df = df[df['word']!=""]
     
-    df.loc[~df["tag"].str.contains('-'+'|-'.join(cfg.LABELS)), "tag"] = 'O'
+    df.loc[~df["tag"].isin(cfg.TAGS), "tag"] = 'O'
 
     enc_tag = preprocessing.LabelEncoder()
-    enc_tag.fit(cfg.TAGS)
+    enc_tag.classes_ = np.array(cfg.TAGS)
+
     df.loc[:, "tag"] = enc_tag.transform(df["tag"])
     
     cates = df.groupby("id")["cate_id"].apply(list).values
@@ -37,12 +38,12 @@ def process_data(data_path):
 
 
 if __name__ == "__main__":
-    cates, sentences, tags, enc_tag = process_data(cfg.DATA_PATH)
     
+    ### Prepare data
+    cates, sentences, tags, enc_tag = process_data(cfg.DATA_PATH)    
     meta_data = {
         "enc_tag": enc_tag
     }
-
     joblib.dump(meta_data, "meta.bin")
 
     (
@@ -55,49 +56,57 @@ if __name__ == "__main__":
     ) = model_selection.train_test_split(cates, sentences, tags, random_state=42, test_size=0.1)
 
     train_dataset = EntityDataset(train_cates, train_sentences, train_tags, cfg)
-
     train_data_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=cfg.TRAIN_BATCH_SIZE, num_workers=4
+        train_dataset, batch_size=cfg.TRAIN_BATCH_SIZE, num_workers=cfg.NUM_WORKERS
     )
 
     valid_dataset = EntityDataset(test_cates, test_sentences, test_tags, cfg)
-
     valid_data_loader = torch.utils.data.DataLoader(
-        valid_dataset, batch_size=cfg.VALID_BATCH_SIZE, num_workers=1
+        valid_dataset, batch_size=cfg.VALID_BATCH_SIZE, num_workers=cfg.NUM_WORKERS
     )
 
+    ### Prepare model
+    # configure model
     device = torch.device(cfg.DEVICE)
     model = TXtract_AVExtraction(cfg)
     model.to(device)
 
-    param_optimizer = list(model.named_parameters())
-    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-    optimizer_parameters = [
-        {
-            "params": [
-                p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": 0.001,
-        },
-        {
-            "params": [
-                p for n, p in param_optimizer if any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": 0.0,
-        },
-    ]
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.LEARNING_RATE, weight_decay=cfg.WEIGHT_DECAY)
+    
+    # param_optimizer = list(model.named_parameters())
+    # no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    # optimizer_parameters = [
+    #     {
+    #         "params": [
+    #             p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
+    #         ],
+    #         "weight_decay": 0.001,
+    #     },
+    #     {
+    #         "params": [
+    #             p for n, p in param_optimizer if any(nd in n for nd in no_decay)
+    #         ],
+    #         "weight_decay": 0.0,
+    #     },
+    # ]
 
-    num_train_steps = int(len(train_sentences) / cfg.TRAIN_BATCH_SIZE * cfg.EPOCHS)
-    optimizer = AdamW(optimizer_parameters, lr=3e-5)
-    scheduler = get_linear_schedule_with_warmup(
-        optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
-    )
+    # optimizer = AdamW(optimizer_parameters, lr=cfg.LEARNING_RATE)
+    # num_train_steps = int(len(train_sentences) / cfg.TRAIN_BATCH_SIZE * cfg.EPOCHS)
+    
+    # scheduler = get_linear_schedule_with_warmup(
+    #     optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
+    # )
+    
+
+    ### Training
 
     best_loss = np.inf
     for epoch in range(cfg.EPOCHS):
-        train_loss = engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
+        train_loss = engine.train_fn(train_data_loader, model, optimizer, device)
         test_loss = engine.eval_fn(valid_data_loader, model, device)
         print(f"Train Loss = {train_loss} Valid Loss = {test_loss}")
+        torch.save(model.state_dict(), 'last_model.bin')
         if test_loss < best_loss:
             torch.save(model.state_dict(), cfg.MODEL_PATH)
             best_loss = test_loss
